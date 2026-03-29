91f98fd55b62aaeb4383f7bc009b2cb0644130da
Amane Godo <amanegodo@gmail.com>
2026-03-29 17:21:08 -0400

    Add: subtree-of-a-binary-tree - submission-6

---
+++ Data Structures & Algorithms/subtree-of-a-binary-tree/submission-6.cpp
@@ -0,0 +1,46 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+class Solution {
+public:
+    bool isSubtree(TreeNode* root, TreeNode* subRoot) {
+        if (!subRoot) {
+            return true;
+        }
+
+        if (!root) {
+            return false;
+        }
+
+        if (isSameTree(root, subRoot)) {
+            return true;
+        }
+
+        return isSubtree(root->left, subRoot) || isSubtree(root->right, subRoot);
+    }
+
+private:
+    bool isSameTree(TreeNode* root, TreeNode* sub) {
+        if (!root && !sub) {
+            return true;
+        }
+
+        if (root && sub && root->val == sub->val) {
+            return isSameTree(root->left, sub->left) && isSameTree(root->right, sub->right);
+        }
+
+        else {
+            return false;
+        }
+    }
+
+};
