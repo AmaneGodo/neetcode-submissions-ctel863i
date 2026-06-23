f21a6cee64db321d2a76eeecf393033e3ee5df50
Amane Godo <amanegodo@gmail.com>
2026-06-23 10:42:16 -0400

    Add: is-palindrome - submission-16

---
+++ Data Structures & Algorithms/is-palindrome/submission-16.py
@@ -0,0 +1,27 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        i = 0
+        j = len(s) - 1
+
+        while i < j:
+            while i < j and not self.isAlNum(s[i].lower()):
+                i += 1
+            
+            while i < j  and not self.isAlNum(s[j].lower()):
+                j -= 1
+
+            if s[i].lower() != s[j].lower():
+                return False
+
+            i += 1
+            j -= 1
+
+        return True
+
+    def isAlNum(self, c):
+        if (ord('a') <= ord(c) <= ord('z')) or (ord('0') <= ord(c) <= ord('9')):
+            return True
+
+        else:
+            return False   
+    
