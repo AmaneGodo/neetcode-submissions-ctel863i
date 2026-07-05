51de5d1b76aca4af80d58312a9e17411544a6d10
Amane Godo <amanegodo@gmail.com>
2026-07-05 18:24:58 -0400

    Add: validate-parentheses - submission-12

---
+++ Data Structures & Algorithms/validate-parentheses/submission-12.py
@@ -0,0 +1,18 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        co_map = {')':'(', ']':'[', '}':'{'}
+
+        stack = []
+
+        for c in s:
+            if stack and c in co_map:
+                if co_map[c] == stack[-1]:
+                    stack.pop()
+
+                else:
+                    return False
+
+            else:
+                stack.append(c)
+
+        return True if not stack else False
