5505a8740a4f302dc6a9a89aeb098617c629c317
Amane Godo <amanegodo@gmail.com>
2026-07-22 13:23:39 -0400

    Add: duplicate-integer - submission-14

---
+++ Data Structures & Algorithms/duplicate-integer/submission-14.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool hasDuplicate(vector<int>& nums) {
+        std::unordered_set<int> hashset;
+
+        for (int n : nums) {
+            if (hashset.count(n)) {
+                return true;
+            }
+
+            hashset.insert(n);
+        }
+
+        return false;
+    }
+};
