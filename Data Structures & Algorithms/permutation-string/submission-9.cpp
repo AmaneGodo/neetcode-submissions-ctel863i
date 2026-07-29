10a625ddfffb2053eaef4604037ebe23f88ada89
Amane Godo <amanegodo@gmail.com>
2026-07-29 17:13:04 -0400

    Add: permutation-string - submission-9

---
+++ Data Structures & Algorithms/permutation-string/submission-9.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    bool checkInclusion(string s1, string s2) {
+        if (s1.length() > s2.length()) {
+            return false;
+        }
+
+        vector<int> count_s1(26, 0);
+        vector<int> count_s2(26, 0);
+
+        for (int i = 0; i < s1.length(); i++) {
+            count_s1[s1[i] - 'a']++;
+            count_s2[s2[i] - 'a']++;
+        }
+
+        if (count_s1 == count_s2) {
+            return true;
+        }
+
+        int l = 0;
+        for (int r = s1.length(); r < s2.length(); r++) {
+            count_s2[s2[l] - 'a']--;
+            l++;
+            count_s2[s2[r] - 'a']++;
+
+            if (count_s1 == count_s2) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+};
